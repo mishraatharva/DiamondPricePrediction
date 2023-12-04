c1b53194a560185149008c57a9a40557c21e17d0
mishraatharva <mishraatharva825@gmail.com>
2023-12-04 19:46:41 +0530

    initial setup contd completed

--- init_setup.sh
+++ init_setup.sh
@@ -1,11 +1,8 @@
 echo [$(date)]: "START"
 
-
 echo [$(date)]: "creating env with python 3.8 version" 
 
-
 conda create --prefix ./env python=3.8 -y
-
 
 echo [$(date)]: "activating the environment" 
 
