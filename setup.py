17ecc32a29fc2bd7d92238e685d5baf7a42db42d
mishraatharva <mishraatharva825@gmail.com>
2023-12-08 15:10:06 +0530

    training pipeline completed

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ def get_requirements(file_path:str)->List[str]:
 setup(
     name='DimondPricePrediction',
     version='0.0.1',
-    author='sunny savita',
-    author_email='sunny.savita@ineuron.ai',
+    author='atharva mishra',
+    author_email='mishraatharva825@gmail.com',
     install_requires=["scikit-learn","pandas","numpy"],
     packages=find_packages()
 )
